ba0e2abaec7073bff3c5de1b12e164c1c6cfb072
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-14 15:29:03 +0200

    snack 2 svolto

--- js/main.js
+++ js/main.js
@@ -29,3 +29,35 @@ console.log(
 // Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const footballTeams = [
+  { nome: "Napoli", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Atalanta", puntiFatti: 0, falliSubiti: 0 },
+];
+
+const footballTeamsOnlyFouls = [];
+
+const generateRandomNumber = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+for (const team of footballTeams) {
+  team.puntiFatti = generateRandomNumber(114);
+  team.falliSubiti = generateRandomNumber(190);
+}
+
+console.log(footballTeams);
+
+for (const team of footballTeams) {
+  const teamOnlyFouls = {
+    nome: team.nome,
+    falliSubiti: team.falliSubiti,
+  };
+
+  footballTeamsOnlyFouls.push(teamOnlyFouls);
+}
+
+console.log(footballTeamsOnlyFouls);
